64b9f6b88523e3248f59e72ac44b7d432e014eef
David Winfield <67267592+david-ryder@users.noreply.github.com>
2023-06-25 23:53:41 -0400

    idk i guess it's cool

---
+++ rest_project/api/views.py
@@ -0,0 +1,22 @@
+from rest_framework import viewsets, response
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def list(self, request):
+        queryset = self.queryset
+
+        title = request.query_params.get('title', None)
+        author = request.query_params.get('author', None)
+
+        if title is not None:
+            queryset = queryset.filter(title__icontains=title)
+
+        if author is not None:
+            queryset = queryset.filter(author__icontains=author)
+
+        serializer = self.serializer_class(queryset, many=True)
+        return response.Response(serializer.data)
